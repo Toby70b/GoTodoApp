c8c5a1666d422e9a88fb7bd1e3e6f42a7f4906a9
Toby <tobypl407@googlemail.com>
2022-07-31 12:05:05 +0100

    Adding the initial REST API, learning stuff

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"TodoApp/models"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+dsadsa
+var Articles []models.Article
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println(writer, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+func returnAllArticles(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(writer).Encode(Articles)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	log.Fatalln(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []models.Article{
+		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
