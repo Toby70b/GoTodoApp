e32cc01e1845f3f9d78a14f3d60030c34b6f8792
Toby <tobypl407@googlemail.com>
2022-08-16 17:58:04 +0100

    remove business logic from controller and request logic from service

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
 )
@@ -20,24 +21,49 @@ func (controller *TodoController) returnAllTodos(writer http.ResponseWriter, req
 	json.NewEncoder(writer).Encode(response)
 }
 
-func returnSingleTodo(writer http.ResponseWriter, request *http.Request) {
+func (controller *TodoController) returnSingleTodo(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint Hit: returnSingleTodo")
+	vars := mux.Vars(request)
+	todoId := vars["id"]
+	response, err := controller.todoService.ReturnSingleTodo(todoId)
+	if err != nil {
+		json.NewEncoder(writer).Encode(err.Error())
+	} else {
+		json.NewEncoder(writer).Encode(response)
+	}
 }
 
-func createNewTodo(writer http.ResponseWriter, request *http.Request) {
+func (controller *TodoController) createNewTodo(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint Hit: createNewTodo")
-
-	//returnJsonResponse(writer, response)
+	reqBody, _ := io.ReadAll(request.Body)
+	var todo models.Todo
+	json.Unmarshal(reqBody, &todo)
+	response, err := controller.todoService.CreateNewTodo(todo)
+	if err != nil {
+		json.NewEncoder(writer).Encode(err.Error())
+	} else {
+		json.NewEncoder(writer).Encode(response)
+	}
 }
 
-func deleteTodo(writer http.ResponseWriter, request *http.Request) {
+func (controller *TodoController) deleteTodo(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint Hit: deleteTodo")
-
-	//returnJsonResponse(writer, response)
+	vars := mux.Vars(request)
+	todoId := vars["todoId"]
+	controller.todoService.DeleteTodo(todoId)
 }
 
-func updateTodo(writer http.ResponseWriter, request *http.Request) {
+func (controller *TodoController) updateTodo(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint Hit: updateTodo")
+	reqBody, _ := io.ReadAll(request.Body)
+	var todo models.Todo
+	json.Unmarshal(reqBody, &todo)
+	response, err := controller.todoService.UpdateTodo(todo)
+	if err != nil {
+		json.NewEncoder(writer).Encode(err.Error())
+	} else {
+		json.NewEncoder(writer).Encode(response)
+	}
 
 }
 
@@ -48,16 +74,16 @@ func handleRequests() {
 	}
 	var controller = TodoController{todoService: services.TodoService{Todos: todos}}
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/todo", createNewTodo).Methods("POST")
+	myRouter.HandleFunc("/todo", controller.createNewTodo).Methods("POST")
+	myRouter.HandleFunc("/todo", controller.updateTodo).Methods("PUT")
 	myRouter.HandleFunc("/todo", controller.returnAllTodos)
-	myRouter.HandleFunc("/todo/{id}", deleteTodo).Methods("DELETE")
-	myRouter.HandleFunc("/todo/{id}", updateTodo).Methods("PUT")
-	myRouter.HandleFunc("/todo/{id}", returnSingleTodo)
+	myRouter.HandleFunc("/todo/{id}", controller.deleteTodo).Methods("DELETE")
+	myRouter.HandleFunc("/todo/{id}", controller.returnSingleTodo)
 	log.Fatalln(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
+	fmt.Println("Rest API v1.0 - Mux Routers")
 	handleRequests()
 }
 
--- services/TodoService.go
+++ services/TodoService.go
@@ -23,50 +23,36 @@ func (service *TodoService) ReturnSingleTodo(id string) (models.Todo, error) {
 	return models.Todo{}, errors.New(fmt.Sprintf("Could not find todo with id [%s]", id))
 }
 
-/*
-func (service *TodoService) createNewTodo(writer http.ResponseWriter, request *http.Request) {
+func (service *TodoService) CreateNewTodo(newTodo models.Todo) (models.Todo, error) {
 	fmt.Println("Endpoint Hit: createNewTodo")
-	reqBody, _ := io.ReadAll(request.Body)
-	var todo models.Todo
-	json.Unmarshal(reqBody, &todo)
-	Todos = append(Todos, todo)
-	response := models.NewResponse(http.StatusCreated, todo)
-	returnJsonResponse(writer, response)
+	for _, todo := range service.Todos {
+		if todo.Id == newTodo.Id {
+			return models.Todo{}, errors.New(fmt.Sprintf("Todo with id [%s] already found", newTodo.Id))
+		}
+	}
+	service.Todos = append(service.Todos, newTodo)
+	return newTodo, nil
 }
 
-func (service *TodoService) deleteTodo(writer http.ResponseWriter, request *http.Request) {
+func (service *TodoService) DeleteTodo(id string) {
 	fmt.Println("Endpoint Hit: deleteTodo")
-	vars := mux.Vars(request)
-	id := vars["id"]
-
-	for i, todo := range Todos {
+	for i, todo := range service.Todos {
 		if todo.Id == id {
 			//Todos equals all values before index (remember slices don't include value at the max index specified)
 			//Plus all the values one index after the found index (remember slices do include the value at the min index)
 			//the ... will pass the slice to the variadic function
-			Todos = append(Todos[:i], Todos[i+1:]...)
+			service.Todos = append(service.Todos[:i], service.Todos[i+1:]...)
 		}
 	}
-
-	response := models.NewResponse(http.StatusOK, nil)
-	returnJsonResponse(writer, response)
 }
 
-func (service *TodoService) updateTodo(writer http.ResponseWriter, request *http.Request) {
+func (service *TodoService) UpdateTodo(newTodo models.Todo) (models.Todo, error) {
 	fmt.Println("Endpoint Hit: updateTodo")
-
-	vars := mux.Vars(request)
-	key := vars["id"]
-
-	reqBody, _ := io.ReadAll(request.Body)
-	var requestTodo models.Todo
-	json.Unmarshal(reqBody, &requestTodo)
-
-	for i, todo := range Todos {
-		if todo.Id == key {
-			Todos[i] = requestTodo
-			response := models.NewResponse(http.StatusOK, Todos[i])
-			returnJsonResponse(writer, response)
+	for i, todo := range service.Todos {
+		if todo.Id == newTodo.Id {
+			service.Todos[i] = newTodo
+			return newTodo, nil
 		}
 	}
-*/
+	return models.Todo{}, errors.New(fmt.Sprintf("Could not find todo with id [%s]", newTodo.Id))
+}
