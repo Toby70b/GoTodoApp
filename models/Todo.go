78403c02a6713eaed1f2fa2a31656d5ffcd1f1bb
Toby <tobypl407@googlemail.com>
2022-08-24 10:36:59 +0100

    Godoc

--- models/Todo.go
+++ models/Todo.go
@@ -1,5 +1,14 @@
 package models
 
+// a Todo item. Composed of the following fields:
+//
+// Id: A unique identifier of the todo item
+//
+// Title: A short description of the todo item
+//
+// Desc: A longer, more detailed description of the todo item
+//
+// Completed: boolean value indicating whether the todo item has been completed or not
 type Todo struct {
 	Id        string `json:"Id"`
 	Title     string `json:"Title"`
