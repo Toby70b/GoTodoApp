ad4ab0cb3994587c6dbf1e478c012fae4c0fe245
Toby <tobypl407@googlemail.com>
2022-08-28 14:27:41 +0100

    Adding unit tests for ReturnSingleTodo method

--- src/test/TodoServiceTest_test.go
+++ src/test/TodoServiceTest_test.go
@@ -9,19 +9,14 @@ import (
 
 var todoService services.TodoService
 
-func setupTest(tb *testing.T) func(tb *testing.T) {
+func setupTest(tb *testing.T) {
 	log.Println("setup test")
 	var todos []models.Todo
 	todoService = services.NewTodoService(todos)
-	// Return a function to teardown the test
-	return func(tb *testing.T) {
-		log.Println("teardown test")
-	}
 }
 
 func TestReturnAllTodosNoTodoFound(t *testing.T) {
-	teardownTest := setupTest(t)
-	defer teardownTest(t)
+	setupTest(t)
 	actualTodos := todoService.ReturnAllTodos()
 	if len(actualTodos) != 0 {
 		t.Error("expected array of length", 0, "but received array of length", len(actualTodos))
@@ -29,9 +24,7 @@ func TestReturnAllTodosNoTodoFound(t *testing.T) {
 }
 
 func TestReturnAllTodosOneTodoFound(t *testing.T) {
-	teardownTest := setupTest(t)
-	defer teardownTest(t)
-
+	setupTest(t)
 	expectedTodo := models.Todo{
 		Id:        "1",
 		Title:     "Example Title",
@@ -51,9 +44,7 @@ func TestReturnAllTodosOneTodoFound(t *testing.T) {
 }
 
 func TestReturnAllTodosMultipleTodosFound(t *testing.T) {
-	teardownTest := setupTest(t)
-	defer teardownTest(t)
-
+	setupTest(t)
 	expectedTodo1 := models.Todo{
 		Id:        "1",
 		Title:     "Example Title",
@@ -79,5 +70,54 @@ func TestReturnAllTodosMultipleTodosFound(t *testing.T) {
 	}
 	if actualTodos[1] != expectedTodo2 {
 		t.Error("expected todo:", expectedTodo2, "but received todo:", actualTodos[1])
+	}
+}
+
+func TestReturnSingleTodoNoTodoFoundEmptyArray(t *testing.T) {
+	setupTest(t)
+	actualTodo, _ := todoService.ReturnSingleTodo("1")
+	if actualTodo != (models.Todo{Completed: false}) {
+		t.Error("expected Todo to be empty, instead was:", actualTodo)
+	}
+}
+
+func TestReturnSingleTodoNoTodoFoundReturnError(t *testing.T) {
+	setupTest(t)
+	_, err := todoService.ReturnSingleTodo("1")
+	if err == nil {
+		t.Error("expected error but no error returned")
+	}
+	if err.Error() != "could not find todo with id [1]" {
+		t.Error("expected error message was not found, instead was:", err.Error())
+	}
+}
+
+func TestReturnSingleTodoNoTodoFoundWrongId(t *testing.T) {
+	setupTest(t)
+	expectedTodo := models.Todo{
+		Id:        "1",
+		Title:     "Example Title",
+		Desc:      "Example Description",
+		Completed: false,
+	}
+	todoService.Todos = append(todoService.Todos, expectedTodo)
+	actualTodo, _ := todoService.ReturnSingleTodo("2")
+	if actualTodo != (models.Todo{Completed: false}) {
+		t.Error("expected Todo to be empty, instead was:", actualTodo)
+	}
+}
+
+func TestReturnSingleTodoTodoFound(t *testing.T) {
+	setupTest(t)
+	expectedTodo := models.Todo{
+		Id:        "1",
+		Title:     "Example Title",
+		Desc:      "Example Description",
+		Completed: false,
+	}
+	todoService.Todos = append(todoService.Todos, expectedTodo)
+	actualTodo, _ := todoService.ReturnSingleTodo("1")
+	if actualTodo != expectedTodo {
+		t.Error("expected todo:", expectedTodo, "but received todo:", actualTodo)
 	}
 }
